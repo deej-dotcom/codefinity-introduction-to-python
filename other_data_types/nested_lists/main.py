61246bdf4cb8be59993bd234f845d839e8273488
deej-dotcom <>
2026-03-09 00:57:12 +0000

    Nested Lists

--- other_data_types/nested_lists/main.py
+++ other_data_types/nested_lists/main.py
@@ -1,9 +1,13 @@
 vegetables = ["tomatoes", "potatoes", "onions"]
 vegetables.remove("onions")
-print(vegetables)
-vegetables.append("carrots")
-print(vegetables)
-vegetables.append("cucumbers")
-print(vegetables)
 
+#check membership before appending
+if "carrots" not in vegetables:
+    vegetables.append("carrots")
+else: print ("Carrots are already in the list")
 
+if "cucumbers" not in vegetables:
+    vegetables.append("cucumbers")
+else: print ("Cucumbers are already in the list")
+vegetables.sort()
+print("Updated Vegetable Inventory:", vegetables)
