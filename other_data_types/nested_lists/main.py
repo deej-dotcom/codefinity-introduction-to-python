7fe604136e77915478a14070ee0dd1d266493a37
deej-dotcom <>
2026-03-09 00:43:50 +0000

    Nested Lists

---
+++ other_data_types/nested_lists/main.py
@@ -0,0 +1,9 @@
+vegetables = ["tomatoes", "potatoes", "onions"]
+vegetables.remove("onions")
+print(vegetables)
+vegetables.append("carrots")
+print(vegetables)
+vegetables.append("cucumbers")
+print(vegetables)
+
+
